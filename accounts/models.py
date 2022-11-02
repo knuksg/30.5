6239002ddb6e6a8061514f5d76f938cae08f896b
knuksg <ksgyo0121@gmail.com>
2022-11-02 10:25:23 +0900

    feat: Add User default image

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ from imagekit.processors import ResizeToFill
 # Create your models here.
 class User(AbstractUser):
     phone = models.CharField(max_length=11, default="None")
-    profile_image = ProcessedImageField(
+    image = ProcessedImageField(
         upload_to="media/",
         blank=True,
         processors=[ResizeToFill(100, 100)],
@@ -17,3 +17,10 @@ class User(AbstractUser):
     like_restaurants = models.ManyToManyField(
         "restaurants.Restaurant", related_name="like_users"
     )
+
+    def profile_image(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+        else:
+            return 'https://postfiles.pstatic.net/MjAyMDExMDFfMTA1/MDAxNjA0MjI4ODc1Mzk0.05ODadJdsa3Std55y7vd2Vm8kxU1qScjh5-3eVJ9T-4g.h7lHansSdReVq7IggiFAc44t2W_ZPTPoZWihfRMB_TYg.JPEG.gambasg/%EC%9C%A0%ED%8A%9C%EB%B8%8C_%EA%B8%B0%EB%B3%B8%ED%94%84%EB%A1%9C%ED%95%84_%ED%8C%8C%EB%9E%91.jpg?type=w773'
+            # return '/static/images/sample.jpg'
