01ab198e830b88da46f7e74a5b3eeac15bf389ff
byeongjin <qudwls7468@gmail.com>
2022-11-03 15:29:34 +0900

    fix

--- restaurants/migrations/0001_initial.py
+++ restaurants/migrations/0001_initial.py
@@ -8,35 +8,56 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Restaurant',
+            name="Restaurant",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=80)),
-                ('address', models.CharField(max_length=80)),
-                ('shop_number', models.CharField(max_length=80)),
-                ('between_pay', models.CharField(max_length=80)),
-                ('opening_time', models.CharField(max_length=80)),
-                ('break_day', models.CharField(max_length=80)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-                ('hits', models.IntegerField(default=0)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=80)),
+                ("address", models.CharField(max_length=80)),
+                ("shop_number", models.CharField(max_length=80)),
+                ("between_pay", models.CharField(max_length=80)),
+                ("opening_time", models.CharField(max_length=80)),
+                ("break_day", models.CharField(max_length=80)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                ("hits", models.IntegerField(default=0)),
             ],
         ),
         migrations.CreateModel(
-            name='Category',
+            name="Category",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('tag1', models.CharField(blank=True, max_length=20)),
-                ('tag2', models.CharField(blank=True, max_length=20)),
-                ('tag3', models.CharField(blank=True, max_length=20)),
-                ('tag4', models.CharField(blank=True, max_length=20)),
-                ('tag5', models.CharField(blank=True, max_length=20)),
-                ('restaurant', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='restaurants.restaurant')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("tag1", models.CharField(blank=True, max_length=20)),
+                ("tag2", models.CharField(blank=True, max_length=20)),
+                ("tag3", models.CharField(blank=True, max_length=20)),
+                ("tag4", models.CharField(blank=True, max_length=20)),
+                ("tag5", models.CharField(blank=True, max_length=20)),
+                (
+                    "restaurant",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="restaurants.restaurant",
+                    ),
+                ),
             ],
         ),
     ]
