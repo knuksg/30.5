a00d3cdfcfc9eff6958f8bee80aba6ad4c233a25
knuksg <ksgyo0121@gmail.com>
2022-11-03 11:21:29 +0900

    feat: 검색 기능 구현 중

--- stories/views.py
+++ stories/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Story
 from .forms import StoryForm
+from restaurants.models import Restaurant
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -46,3 +49,17 @@ def update(request, pk):
         'form': form
     }
     return render(request, 'stories/update.html', context)
+
+def search_test(request):
+    page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')  # 검색어
+    question_list = Restaurant.objects.order_by('-created_at')
+    if kw:
+        question_list = question_list.filter(
+            Q(name__icontains=kw) |  # 이름 검색
+            Q(address__icontains=kw)  # 주소 검색
+        ).distinct()
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
+    return render(request, 'stories/search_test.html', context)  
