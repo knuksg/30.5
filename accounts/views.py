6f2041514374b1ef394c3566fd4c81fde3b63d6d
byeongjin <qudwls7468@gmail.com>
2022-11-03 10:01:13 +0900

    temp/login

--- accounts/views.py
+++ accounts/views.py
@@ -6,67 +6,65 @@ from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 
+
 def index(request):
     users = get_user_model().objects.all()
-    context = {
-        'users':users
-    }
-    return render(request, 'accounts/index.html', context)
+    context = {"users": users}
+    return render(request, "accounts/index.html", context)
+
 
 def signup(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)    
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
-            return redirect('accounts:index')
+            return redirect("restaurants:main")
     else:
         form = CustomUserCreationForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'accounts/signup.html', context)
+    context = {"form": form}
+    return render(request, "accounts/signup.html", context)
+
 
 def detail(request, user_pk):
     user = get_user_model().objects.get(pk=user_pk)
     context = {
-        'user': user,
+        "user": user,
     }
-    return render(request, 'accounts/detail.html', context)
+    return render(request, "accounts/detail.html", context)
+
 
 @login_required
 def update(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomUserChangeForm(request.POST, request.FILES, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect('accounts:detail', request.user.pk)
+            return redirect("accounts:detail", request.user.pk)
     else:
         form = CustomUserChangeForm(instance=request.user)
-    context = {
-        'form':form
-    }
-    return render(request, 'accounts/update.html', context)
+    context = {"form": form}
+    return render(request, "accounts/update.html", context)
+
 
 def delete(request):
     request.user.delete()
     auth_logout(request)
-    return redirect('accounts:index')
+    return redirect("restaurants:main")
+
 
 def login(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            return redirect(request.GET.get('next') or 'accounts:index')
+            return redirect(request.GET.get("next") or "restaurants:main")
     else:
         form = AuthenticationForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'accounts/login.html', context)
+    context = {"form": form}
+    return render(request, "accounts/login.html", context)
+
 
 def logout(request):
     auth_logout(request)
-    return redirect('accounts:index')
-
+    return redirect("restaurants:main")
--- static/js/main.js
+++ static/js/main.js
@@ -19,3 +19,22 @@ function navigo() {
   }
 }
 navigo()
+
+// Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+});
+
+
+// Nav Toggle
+const navs = document.querySelector('.nav-toggle')
+const navbarToggleBtn = document.querySelector('.toggle-btn')
+navbarToggleBtn.addEventListener('click', () => {
+    navs.classList.toggle('open');
+});
