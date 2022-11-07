588251dd7a8015ed0ad7167e5d01354af49f4632
knuksg <ksgyo0121@gmail.com>
2022-11-07 17:20:59 +0900

    feat: main_list 수정 완료

--- main/views.py
+++ main/views.py
@@ -6,11 +6,31 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    korea_restaurants = Tag.objects.filter(name="한식")
-    china_restaurants = Tag.objects.filter(name="중식")
-    japan_restaurants = Tag.objects.filter(name="일식")
-    western_restaurants = Tag.objects.filter(name="양식")
-    school_restaurants = Tag.objects.filter(name="분식")
+    korea_restaurants = Restaurant.objects.filter(
+            Q(tags__name="한식") |
+            Q(name__icontains="한식") |
+            Q(subtext__icontains="한식")
+            )[:5] # 5개까지만 보여줌.
+    china_restaurants = Restaurant.objects.filter(
+            Q(tags__name="중식") |
+            Q(name__icontains="중식") |
+            Q(subtext__icontains="중식")
+            )[:5] # 5개까지만 보여줌.
+    japan_restaurants = Restaurant.objects.filter(
+            Q(tags__name="일식") |
+            Q(name__icontains="일식") |
+            Q(subtext__icontains="일식")
+            )[:5] # 5개까지만 보여줌.
+    western_restaurants = Restaurant.objects.filter(
+            Q(tags__name="양식") |
+            Q(name__icontains="양식") |
+            Q(subtext__icontains="양식")
+            )[:5] # 5개까지만 보여줌.
+    school_restaurants = Restaurant.objects.filter(
+            Q(tags__name="분식") |
+            Q(name__icontains="분식") |
+            Q(subtext__icontains="분식")
+            )[:5] # 5개까지만 보여줌.
     restaurants = sorted(Restaurant.objects.all(), key=lambda a: a.grade)
     stories = Story.objects.order_by("-pk")[:3]
     context = {
@@ -32,12 +52,13 @@ def search(request):
     tag = Tag.objects.filter(name__icontains=kw)
     if kw:
         restaurant_list = restaurant_list.filter(
-            Q(name__icontains=kw)
-            | Q(address__icontains=kw)  # 이름 검색
-            | Q(tags__in=tag)  # 주소 검색  # 태그 검색
+            Q(name__icontains=kw) | # 이름 검색
+            Q(address__icontains=kw) | # 주소 검색
+            Q(subtext__icontains=kw) | # 서브 텍스트 검색
+            Q(tags__in=tag) # 태그 검색
         ).distinct()
 
-    paginator = Paginator(restaurant_list, 10)  # 페이지당 10개씩 보여주기
+    paginator = Paginator(restaurant_list, 9)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
     context = {"restaurant_list": page_obj, "page": page, "kw": kw}
     return render(request, "main/search.html", context)
